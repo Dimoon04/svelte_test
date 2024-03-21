d78b6ce7b0149d812143fe50dcc2dbe7a786ce38
dmitr <dmitriyoorzhak2004@gmail.com>
2024-03-21 13:19:05 +0700

    add blog

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+    {
+        id:1,
+        title: 'My First Blog Post',
+        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, esse. Mollitia maxime nostrum voluptate eveniet dicta reiciendis suscipit consectetur! Dolor explicabo temporibus sit error quas tempore architecto magni provident iste.'
+        
+    },
+    {
+        id:2,
+        title: 'My Second Blog Post',
+        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, esse. Mollitia maxime nostrum voluptate eveniet dicta reiciendis suscipit consectetur! Dolor explicabo temporibus sit error quas tempore architecto magni provident iste.'
+        
+    }
+];
